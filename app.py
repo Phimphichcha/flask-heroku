11921a555696a0d4e7e3d85ec19ad6e45aecef2e
Phimphichcha <89175207+Phimphichcha@users.noreply.github.com>
2021-09-02 10:02:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello Flask-Herok"
+    return "Hello พิมพิชชา คำภากุล เลขที่21 ห้อง4/5"
 
 @app.route('/api', methods=['GET'])
 def get_api():
